185f3e96a7ca3ed2e045237901ca94577cd47645
menza <menyuske@gmail.com>
2015-08-24 22:29:58 +0100

    splitting parser monad and roman numeral

--- RomanNumeral/Monad.hs
+++ RomanNumeral/Monad.hs
@@ -3,17 +3,6 @@ module RomanNumeral.Monad where
 import Control.Applicative hiding (optional)
 import RomanNumeral.ParserMonad
 
-letter     :: Char -> Int -> Parser Int
-letter c i = do { x <- char c; return i }
-
-i = letter 'I' 1
-v = letter 'V' 5
-x = letter 'X' 10
-l = letter 'L' 50
-c = letter 'C' 100
-d = letter 'D' 500
-m = letter 'M' 1000
-
 minus   :: Parser Int -> Parser Int
 minus p = do { a <- p; return (-a) }
 
@@ -32,16 +21,30 @@ upTo3 p = sumP $ upTo 3 p
   where sumP   :: Parser [Int] -> Parser Int
         sumP p = do { as <- p ; return (sum as)}
 
+letter     :: Char -> Int -> Parser Int
+letter c i = do { x <- char c; return i }
+
+i, v, x, l, c, d, m :: Parser Int
+i = letter 'I' 1
+v = letter 'V' 5
+x = letter 'X' 10
+l = letter 'L' 50
+c = letter 'C' 100
+d = letter 'D' 500
+m = letter 'M' 1000
+
 romanNumeralGroup       :: Parser Int -> Parser Int -> Parser Int -> Parser Int
-romanNumeralGroup i v x = fourOrNine <|> fiveAndOnes
-  where fourOrNine = minus i .> ( v |. x )
-        fiveAndOnes = optional v .> upTo3 i
+romanNumeralGroup unit five ten = four <|> nine <|> fiveAndUnits
+  where four = minus unit .> five
+        nine = minus unit .> ten 
+        fiveAndUnits = optional five .> upTo3 unit
 
 ones = romanNumeralGroup i v x
 tens = romanNumeralGroup x l c
 hundreds = romanNumeralGroup c d m
+thousands = upTo3 m
 
 romanNumeral :: Parser Int
-romanNumeral = upTo3 m .> optional hundreds .> optional tens .> optional ones 
+romanNumeral = optional thousands .> optional hundreds .> optional tens .> optional ones 
 
 
